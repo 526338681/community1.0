fc820da29dc0fc23eb12a833347b0a06e6d49a18
fanchunming <526338681@qq.com>
2022-01-21 21:47:31 +0800

    第八次提交：忘记密码功能

--- src/main/java/com/fan/community/util/CommunityUtil.java
+++ src/main/java/com/fan/community/util/CommunityUtil.java
@@ -2,10 +2,12 @@ package com.fan.community.util;
 
 
 
+import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.util.DigestUtils;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 import java.util.UUID;
 
 public class CommunityUtil {
@@ -21,4 +23,24 @@ public class CommunityUtil {
         //spring自带的工具，加密成16进制的字符串
         return DigestUtils.md5DigestAsHex(key.getBytes());
     }
+    public static String getJSONString(int code, String msg, Map<String, Object> map) {
+        JSONObject json = new JSONObject();
+        json.put("code", code);
+        json.put("msg", msg);
+        if (map != null) {
+            for (String key : map.keySet()) {
+                json.put(key, map.get(key));
+            }
+        }
+        return json.toJSONString();
+    }
+
+    public static String getJSONString(int code, String msg) {
+        return getJSONString(code, msg, null);
+    }
+
+    public static String getJSONString(int code) {
+        return getJSONString(code, null, null);
+    }
+
 }
