726d665fabfef1d7160b88479cdb9190d5bf5dec
fanchunming <526338681@qq.com>
2022-01-21 21:19:07 +0800

    第七次提交：上传头像 & 登录状态检测 & 修改密码（待定）功能

--- src/main/java/com/fan/community/config/WebMvcConfig.java
+++ src/main/java/com/fan/community/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.fan.community.config;
 
 import com.fan.community.controller.interceptor.AlphaInterceptor;
+import com.fan.community.controller.interceptor.LoginRequiredInterceptor;
 import com.fan.community.controller.interceptor.LoginTicketInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Autowired
     private LoginTicketInterceptor loginTicketInterceptor;
+
+    @Autowired
+    private LoginRequiredInterceptor loginRequiredInterceptor;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(alphaInterceptor)
@@ -21,6 +25,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addPathPatterns("/register","/login");
 
         registry.addInterceptor(loginTicketInterceptor)
+                .excludePathPatterns("/**/*.css","/**/*.js","/**/*.jpg","/**/*.jpeg");
+
+        registry.addInterceptor(loginRequiredInterceptor)
                 .excludePathPatterns("/**/*.css","/**/*.js","/**/*.jpg","/**/*.jpeg");
 
     }
