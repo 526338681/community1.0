09881b48aeba52c9e096a389070fa7ef714161bd
fanchunming <526338681@qq.com>
2022-01-29 21:27:11 +0800

    第14次提交：私信列表 功能

--- src/test/java/com/fan/community/MapperTests.java
+++ src/test/java/com/fan/community/MapperTests.java
@@ -2,9 +2,11 @@ package com.fan.community;
 
 import com.fan.community.dao.DiscussPostMapper;
 import com.fan.community.dao.LoginTicketMapper;
+import com.fan.community.dao.MessageMapper;
 import com.fan.community.dao.UserMapper;
 import com.fan.community.entity.DiscussPost;
 import com.fan.community.entity.LoginTicket;
+import com.fan.community.entity.Message;
 import com.fan.community.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,7 +26,8 @@ public class MapperTests {
     private DiscussPostMapper discussPostMapper;
     @Autowired
     private LoginTicketMapper loginTicketMapper;
-
+    @Autowired
+    private MessageMapper messageMapper;
 
     @Test
     public void testselectById(){
@@ -49,6 +52,7 @@ public class MapperTests {
         loginTicket.setExpired(new Date(System.currentTimeMillis() + 1000 * 60 * 10));
         loginTicketMapper.insertLoginTicket(loginTicket);
     }
+
     //org.mybatis.spring.MyBatisSystemException: nested exception is org.apache.ibatis.reflection.ReflectionException: Error instantiating interface com.fan.community.dao.LoginTicketMapper with invalid types () or values (). Cause: java.lang.NoSuchMethodException: com.fan.community.dao.LoginTicketMapper.<init>()
 //    @Test
 //    public void testSelectByTicket(){
@@ -58,4 +62,21 @@ public class MapperTests {
 //        loginTicket = loginTicketMapper.selectByTicket("abc");
 //        System.out.println(loginTicket);
 //    }
+    @Test
+    public void testSelectLetter() {
+        List<Message> messages = messageMapper.selectConversations(111, 0, 20);
+        for (Message message : messages) {
+            System.out.println(message);
+        }
+        int count = messageMapper.selectConversationCount(111);
+        System.out.println(count);
+        List<Message> list = messageMapper.selectLetters("111_112", 0, 10);
+        for (Message message : list) {
+            System.out.println(message);
+        }
+        int i = messageMapper.selectLetterCount("111_112");
+        System.out.println(i);
+        int selectLetterUnreadCount = messageMapper.selectLetterUnreadCount(131, "111_131");
+        System.out.println(selectLetterUnreadCount);
+    }
 }
